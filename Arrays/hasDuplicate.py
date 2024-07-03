55fefc5ce632eafb87f105f5d0ffec201fb1452f
LaBasura <35500586+LaBasura@users.noreply.github.com>
2024-07-03 16:04:15 -0700

    Delete Arrays/hasDuplicate.py

--- Arrays/hasDuplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        checker = {}
-        for i in nums:
-            if i in checker:
-                return True
-            else:
-                checker[i] = 0
-        return False
